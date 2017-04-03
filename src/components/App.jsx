dc1a90e08c57f2d7d08f3b8c1043240f41baf923
julien <julien@julien-laptop.home>
2017-04-03 16:07:05 +0200

    Cedric setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
 import styles from '../styles/app'
 
-import React from 'react'
+import React, { Component } from 'react'
 import { translate } from '../lib/I18n'
 import classNames from 'classnames'
 
-const App = ({ t }) => (
-  <h1 className={classNames(styles['title'])}>{ t('App.welcome') }</h1>
-)
+const fetchExample = () => {
+  return Promise.resolve(['1', '2'])
+}
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFetching: true
+    }
+    fetchExample().then(() => this.setState({ isFetching: false }))
+  }
+
+  render() {
+    const { isFetching } = this.state
+    return (
+      <div>
+        <input type='text' value={isFetching ? 'Fetching' : 'Fetched'} />
+      </div>
+    )
+  }
+}
 
 export default translate()(App)
